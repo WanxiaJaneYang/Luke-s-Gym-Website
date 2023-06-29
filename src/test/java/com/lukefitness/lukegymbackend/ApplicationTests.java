3e30507959b3550e6dd6cf89ec3aeeb1afb3ca80
WanxiaJaneYang <1016887513@qq.com>
2023-06-29 14:07:06 +0930

    backend setup

---
+++ src/test/java/com/lukefitness/lukegymbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukefitness.lukegymbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
