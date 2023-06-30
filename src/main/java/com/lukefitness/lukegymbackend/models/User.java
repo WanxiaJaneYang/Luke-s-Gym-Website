8f6eefbb8449ff0ed6238581491e271f5047c658
WanxiaJaneYang <1016887513@qq.com>
2023-06-30 20:37:04 +0930

    modifying database and server setting adding ssl certificate adding ResultCodeEnum

--- src/main/java/com/lukefitness/lukegymbackend/models/User.java
+++ src/main/java/com/lukefitness/lukegymbackend/models/User.java
@@ -1,21 +1,4 @@
 package com.lukefitness.lukegymbackend.models;
 
-import org.hibernate.usertype.UserType;
-
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String username;
-    private String password; // In a real application, make sure to hash this
-    private String email;
-
-    @Enumerated(EnumType.STRING)
-    private UserType userType; // Enum UserType will be created separately
-
 }
