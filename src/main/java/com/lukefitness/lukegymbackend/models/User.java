3c71205067e137ab0c65597f0abcaffce64e2f81
WanxiaJaneYang <1016887513@qq.com>
2023-06-30 23:58:06 +0930

    adding jwt util for token generating and token decode modify user to suite the current database design

--- src/main/java/com/lukefitness/lukegymbackend/models/User.java
+++ src/main/java/com/lukefitness/lukegymbackend/models/User.java
@@ -1,4 +1,15 @@
 package com.lukefitness.lukegymbackend.models;
 
+import lombok.Data;
+
+@Data
 public class User {
+    /*with lombok, we don't need to write getters and setters for the fields
+    * we just need to write the fields and lombok will generate the getters and setters for us
+     */
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private String user_type;
 }
---
+++ src/main/java/com/lukefitness/lukegymbackend/utils/JWTUtils.java
@@ -0,0 +1,42 @@
+package com.lukefitness.lukegymbackend.utils;
+
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JWTUtils {
+    private static String secret;
+    private static Integer expirationTime;
+
+    public static void setSecret(String secret) {
+        JWTUtils.secret = secret;
+    }
+
+    public static void setExpirationTime(Integer expirationTime) {
+        JWTUtils.expirationTime = expirationTime;
+    }
+
+    public static String getToken(String userId,String userName, String userType) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, expirationTime);
+
+        JWTCreator.Builder builder= com.auth0.jwt.JWT.create();
+        Map<String,String> payload =new HashMap<>();
+        payload.put("userId",userId);
+        payload.put("userName",userName);
+        payload.put("userRole",userType);
+        payload.forEach(builder::withClaim);
+        return builder.withExpiresAt(calendar.getTime()).sign(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret));
+    }
+
+    public static DecodedJWT verifyToken(String token) {
+        return com.auth0.jwt.JWT.require(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret)).build().verify(token);
+    }
+}
