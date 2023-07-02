b63af4423ae80d6f3bcb9c4468230165bb7a1003
WanxiaJaneYang <1016887513@qq.com>
2023-07-03 00:20:35 +0930

    deployed swagger successfully

--- src/main/java/com/lukefitness/lukegymbackend/Application.java
+++ src/main/java/com/lukefitness/lukegymbackend/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @MapperScan("com.lukefitness.lukegymbackend.dao")
 public class Application {
