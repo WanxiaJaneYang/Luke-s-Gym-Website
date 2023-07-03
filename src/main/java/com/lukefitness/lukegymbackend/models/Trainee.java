ee44a36b098b19ebdce3115c6217f32588e9fa73
WanxiaJaneYang <1016887513@qq.com>
2023-07-03 17:00:50 +0930

    delete user-test api functions add trainee model

---
+++ src/main/java/com/lukefitness/lukegymbackend/models/Trainee.java
@@ -0,0 +1,11 @@
+package com.lukefitness.lukegymbackend.models;
+
+import lombok.Data;
+
+@Data
+public class Trainee {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+}
