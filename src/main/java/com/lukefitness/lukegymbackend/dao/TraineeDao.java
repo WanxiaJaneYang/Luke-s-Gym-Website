193ac0fb7faf6c5e0aabaaa1a93cec4dfbea2b57
WanxiaJaneYang <1016887513@qq.com>
2023-07-04 19:57:09 +0930

    finish trainee register and login api

---
+++ src/main/java/com/lukefitness/lukegymbackend/dao/TraineeDao.java
@@ -0,0 +1,11 @@
+package com.lukefitness.lukegymbackend.dao;
+
+import com.lukefitness.lukegymbackend.models.Trainee;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TraineeDao {
+    Trainee getTraineeByUsername(String username);
+    int traineeRegister(Trainee trainee);
+
+}
--- src/main/java/com/lukefitness/lukegymbackend/models/Trainee.java
+++ src/main/java/com/lukefitness/lukegymbackend/models/Trainee.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Trainee {
     private int id;
+    private int trainer_id;
     private String username;
     private String password;
     private String email;
