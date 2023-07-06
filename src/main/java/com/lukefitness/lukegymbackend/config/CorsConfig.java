e10bb347a0451f79eca8b4a58c706d136a758ef4
WanxiaJaneYang <1016887513@qq.com>
2023-07-06 20:12:52 +0930

    debugged cors config

--- src/main/java/com/lukefitness/lukegymbackend/config/CorsConfig.java
+++ src/main/java/com/lukefitness/lukegymbackend/config/CorsConfig.java
@@ -1,14 +1,16 @@
 package com.lukefitness.lukegymbackend.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-    public WebMvcConfigurer corsConfigurer(){
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                         .allowedHeaders("*")
