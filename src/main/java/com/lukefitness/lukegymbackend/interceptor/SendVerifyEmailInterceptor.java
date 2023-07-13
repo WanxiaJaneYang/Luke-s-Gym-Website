7c587a3b5bf3008b0f47d001cc118f2de1b5c1e1
WanxiaJaneYang <1016887513@qq.com>
2023-07-13 17:03:28 +0930

    allow preflight for all interceptors

--- src/main/java/com/lukefitness/lukegymbackend/interceptor/SendVerifyEmailInterceptor.java
+++ src/main/java/com/lukefitness/lukegymbackend/interceptor/SendVerifyEmailInterceptor.java
@@ -7,12 +7,16 @@ import com.lukefitness.lukegymbackend.utils.UrlIdExtractor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class SendVerifyEmailInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)  {
+        if (RequestMethod.OPTIONS.toString().equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
         //
         String token;
         try{
