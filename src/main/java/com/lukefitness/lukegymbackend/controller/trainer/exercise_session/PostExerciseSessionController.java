ecfb4615e4181cd25b3ad0eaecaf0e2ebb574c2d
WanxiaJaneYang <1016887513@qq.com>
2023-07-19 15:53:08 +0930

    adding annotation for swagger ui for two api, grouping the api into different pages

--- src/main/java/com/lukefitness/lukegymbackend/controller/trainer/exercise_session/PostExerciseSessionController.java
+++ src/main/java/com/lukefitness/lukegymbackend/controller/trainer/exercise_session/PostExerciseSessionController.java
@@ -6,12 +6,14 @@ import com.lukefitness.lukegymbackend.service.ExerciseSessionService;
 import com.lukefitness.lukegymbackend.utils.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trainer/{trainerId}/{cardId}/exercise_session")
+@Tag(name="Trainer-Exercise-Session")
 public class PostExerciseSessionController {
     @Autowired
     ExerciseSessionService exerciseSessionService;
